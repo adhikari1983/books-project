d6531cc7cf6a65ed96a49800024a20b9585ac28d
adhikari1983 <91963837+adhikari1983@users.noreply.github.com>
2025-07-15 20:10:44 -0400

    One class added

---
+++ src/main/java/com/mybookapp/service/DemoClass.java
@@ -0,0 +1,14 @@
+/**
+ * DemoClass$.java
+ * <p>
+ * Description: TODO - Describe this class
+ * <p>
+ * Created by Kiran Adhikari on 7/15/2025
+ */
+package com.mybookapp.service;
+
+public class DemoClass {
+    public static void main(String[] args) {
+        System.out.println("This line was added on day two.......");
+    }
+}
