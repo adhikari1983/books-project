46b6886322847a3919e925cac190b1a6324f1024
adhikari1983 <91963837+adhikari1983@users.noreply.github.com>
2025-07-15 20:16:05 -0400

    statement deleted

--- src/main/java/com/mybookapp/service/DemoClass.java
+++ src/main/java/com/mybookapp/service/DemoClass.java
@@ -9,7 +9,7 @@ package com.mybookapp.service;
 
 public class DemoClass {
     public static void main(String[] args) {
-        System.out.println("This line was added on day two.......");
+
         System.out.println("This line was added on day two.......");
     }
 }
